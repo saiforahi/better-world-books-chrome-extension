8e471d73aef5ec59acc1d03d4e60abd5217af413
Shaif Azad <shaif.rahi@northsouth.edu>
2021-03-24 23:51:01 +0600

    fully functional

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const axios=require('axios')
+import axios from 'axios';
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log('Hello from the background')
 
@@ -6,16 +6,55 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     file: 'content-script.js',
   });
   if(request.message=='check_isbn'){
-    axios.post('https://ps.betterworldbooks.com/screen/',JSON.stringify({isbn: request.isbn, accountName:'acirassi_books',user:'acirassi_books', password:'aC1r@SSi'}),{headers: {"Access-Control-Allow-Origin": "*"}}).then(res=>{
-      console.log(res.data)
-      if(res.data.isAccepted===true){
-        chrome.tabs.executeScript(sender.tab.id,{
-          file:'js/play-beep.js'
-        })
-      }
-    }).catch(error=>{
-      console.log(error)
+    console.log('given isbn: ',request.isbn)
+    console.log('json param',JSON.stringify({isbn: request.isbn, accountName:'acirassi_books',user:'acirassi_books', password:'aC1ra$$i'}))
+    // let xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState === 4 && xhr.status===200){
+    //       console.log(xhr.responseText)
+    //       chrome.tabs.executeScript(sender.tab.id,{
+    //         file:'js/play-beep.js'
+    //       })
+    //     }
+    // };
+    // xhr.open('POST', 'https://ps.betterworldbooks.com/screen/',false);
+    // xhr.send(JSON.stringify({isbn: request.isbn, accountName:"acirassi_books",user:"acirassi_books", password:"aC1ra$$i"}));
+    // axios.post('https://ps.betterworldbooks.com/screen/',JSON.stringify({isbn: request.isbn, accountName:"Accirassi Books, LTD.",user:"acirassi_books", password:"aC1ra$$i"}),{headers: {"Access-Control-Allow-Origin": "*"}}).then(res=>{
+    //   console.log('api response: ',res.data)
+    //   if(res.data.isAccepted===true){
+    //     chrome.tabs.executeScript(sender.tab.id,{
+    //       file:'js/play-beep.js'
+    //     })
+    //   }
+    // }).catch(error=>{
+    //   console.log(error)
+    // })
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('accountName','acirassi_books')
+    bodyFormData.append('user','acirassi_books')
+    bodyFormData.append('password','aC1ra$$i')
+    bodyFormData.append('isbn',request.isbn)
+
+    axios({
+      method: "post",
+      url: "https://ps.betterworldbooks.com/screen/",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
     })
+      .then(function (response) {
+        //handle success
+        console.log(response.data.isAccepted===true);
+        if(response.data.isAccepted===true){
+          chrome.tabs.executeScript(sender.tab.id,{
+            file:'js/play-beep.js'
+          })
+        }
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
   }
 })
 
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,6 +1,12 @@
 console.log('Hello from the content-script')
 if(document.getElementById('asinInputBox')!==undefined){
-    document.getElementById('asinInputBox').addEventListener('change',function($event){
-        chrome.runtime.sendMessage({message:'check_isbn',isbn:$event.target.value})
+    document.getElementById('asinInputBox').addEventListener('keyup',function($event){
+        if ($event.key === "Enter") {
+            console.log('event target value',$event.target.value)
+            // Cancel the default action, if needed
+            $event.preventDefault();
+            // Trigger the button element with a click
+            chrome.runtime.sendMessage({message:'check_isbn',isbn:$event.target.value})
+        }
     })
 }
